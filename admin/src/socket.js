903cbde4b17f4879e5ba2f6dffb3f93d6a2542db
Bhaskarsingh123 <singh001bhaskar@gmail.com>
2026-08-18 10:44:52 +0530

    admin socket cjange

--- admin/src/socket.js
+++ admin/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:4000")
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  reconnection: false
+});
 
 export default socket
